10fa25e38905f6e5a7bc76f4a3031211b4af8de9
mrsaanum <christoffer.saanum@gmail.com>
2025-01-12 10:48:19 +0100

    adding all files

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>nu skriver vi kod
+        </h1>
+      </main>
+  );
+}
