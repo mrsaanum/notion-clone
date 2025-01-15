9b41604e7f82f2e41cead7bac94a7a519a958164
mrsaanum <christoffer.saanum@gmail.com>
2025-01-15 11:40:32 +0100

    DB setup and new doc funkitons

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,8 +1,23 @@
+'use client'
+
+import { useRouter } from "next/navigation";
 import { Button } from "./ui/button"
+import { useTransition } from "react";
+import { createNewDocument } from "@/actions/actions";
 
 function NewDocumentButton() {
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
+  const handleCreateNewDocument = () => {
+    startTransition(async () => {
+      // Create a new document
+      const { docId } = await createNewDocument();
+      router.push(`/doc/${docId}`);
+    });
+  };
   return (
-    <div><Button>New Document</Button></div>
+    <div><Button onClick={handleCreateNewDocument} disabled={isPending}>{isPending ? "Skapar nytt..." : "Nytt dokument"}</Button></div>
   )
 }
 export default NewDocumentButton
